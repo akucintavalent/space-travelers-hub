336db440adf16c8f3068e4bf0e9fffe989bf2095
Debas Gebreslasie <65129228+Debas-31@users.noreply.github.com>
2022-01-25 15:34:47 +0300

    Fetch missions

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,18 +1,32 @@
 import { Route, Routes, BrowserRouter } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { getMissions, removeAllMissions } from '../../redux/missions/missions';
 import Missions from '../missions/Missions';
 import MyProfile from '../my_profile/MyProfile';
 import Navbar from '../navbar/Navbar';
 import Rockets from '../rockets/Rockets';
 
-const App = () => (
-  <BrowserRouter>
-    <Navbar />
-    <Routes>
-      <Route path="/" element={<MyProfile />} />
-      <Route path="rockets" element={<Rockets />} />
-      <Route path="missions" element={<Missions />} />
-    </Routes>
-  </BrowserRouter>
-);
+const App = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getMissions());
+    return () => {
+      dispatch(removeAllMissions());
+    };
+  }, []);
+
+  const missions = useSelector((state) => state.missionsReducer);
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<MyProfile />} />
+        <Route path="rockets" element={<Rockets />} />
+        <Route path="missions" element={<Missions missions={missions} />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
 export default App;
---
+++ src/redux/configureStore.js
@@ -0,0 +1,14 @@
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import missionsReducer from './missions/missions';
+
+const reducer = combineReducers({
+  missionsReducer,
+});
+
+const store = createStore(
+  reducer,
+  applyMiddleware(thunk),
+);
+
+export default store;
