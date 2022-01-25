e55dc6deddb8bb804e43260844ef8ba7a12b2bfb
Bohdan Shcherbak <bogdan.szczerbak@gmail.com>
2022-01-25 09:48:34 +0200

    Style header (Navbar)

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -3,11 +3,15 @@ import planet from './planet.png';
 import styles from './Navbar.module.css';
 
 const Navbar = () => (
-  <header>
-    <img className={styles.logo} src={planet} alt="Logo (planet)" />
-    <nav>
+  <header className={styles.Header}>
+    <div className={styles.LogoAndTitle}>
+      <img className={styles.logo} src={planet} alt="Logo (planet)" />
+      <h1>Space Travelers&apos; Hub</h1>
+    </div>
+    <nav className={styles.Nav}>
       <NavLink to="rockets" className={({ isActive }) => (isActive ? styles.NavLinkActive : styles.NavLinkInactive)}>Rockets</NavLink>
       <NavLink to="missions" className={({ isActive }) => (isActive ? styles.NavLinkActive : styles.NavLinkInactive)}>Missions</NavLink>
+      |
       <NavLink to="/" className={({ isActive }) => (isActive ? styles.NavLinkActive : styles.NavLinkInactive)}>My Profile</NavLink>
     </nav>
   </header>
