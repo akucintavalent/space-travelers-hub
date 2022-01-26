ceae3a1ba8e92eed2d8b8db9cd44a728b0edbac5
Debas Gebreslasie <65129228+Debas-31@users.noreply.github.com>
2022-01-26 13:13:02 +0300

    Display joined missions in my portfolio

--- src/components/my_profile/MyProfile.js
+++ src/components/my_profile/MyProfile.js
@@ -1,7 +1,41 @@
-const MyProfile = () => (
-  <div>
-    My profile
-  </div>
-);
+import { useSelector } from 'react-redux';
+import './MyProfile.css';
+
+const MyProfile = () => {
+  const missions = useSelector((state) => state.missionsReducer);
+  return (
+    <div className="TablesContainer">
+      <table>
+        <thead>
+          <tr>
+            <th>My Missions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {missions
+            .filter((mission) => mission.reserved)
+            .map((mission) => (
+              <tr key={mission.mission_id}>
+                <td>{mission.mission_name}</td>
+              </tr>
+            ))}
+        </tbody>
+      </table>
+
+      <table>
+        <thead>
+          <tr>
+            <th>My Rockets</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>My Rockets</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+};
 
 export default MyProfile;
