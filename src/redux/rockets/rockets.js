a73607333fe9e3b61bf603bc212e6dc5979a4b18
Bohdan Shcherbak <bogdan.szczerbak@gmail.com>
2022-01-26 10:52:31 +0200

    Implement rocket booking cancelation - Actions

--- src/redux/rockets/rockets.js
+++ src/redux/rockets/rockets.js
@@ -4,6 +4,7 @@ import axios from 'axios';
 const ADD_ROCKET = 'space-travelers-hub/rockets/ADD_ROCKET';
 const REMOVE_ALL_ROCKETS = 'space-travelers-hub/rockets/REMOVE_ALL_ROCKETS';
 const RESERVE_ROCKET = 'space-travelers-hub/rockets/RESERVE_ROCKET';
+const CANCEL_ROCKET_BOOKING = 'space-travelers-hub/rockets/CANCEL_ROCKET_BOOKING';
 
 const initialState = [];
 
@@ -16,6 +17,11 @@ export const removeAllRockets = () => ({ type: REMOVE_ALL_ROCKETS });
 
 export const reserveRocket = (payload) => ({
   type: RESERVE_ROCKET,
+  id: payload.id,
+});
+
+export const cancelRocketBooking = (payload) => ({
+  type: CANCEL_ROCKET_BOOKING,
   id: payload.id,
 });
 
@@ -51,6 +57,11 @@ const reducer = (state = initialState, action) => {
       return state.map((rocket) => {
         if (rocket.id !== action.id) return rocket;
         return { ...rocket, reserved: true };
+      });
+    case CANCEL_ROCKET_BOOKING:
+      return state.map((rocket) => {
+        if (rocket.id !== action.id) return rocket;
+        return { ...rocket, reserved: false };
       });
     default:
       return state;
